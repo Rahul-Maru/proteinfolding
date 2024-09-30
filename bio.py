12cfe9c6389f5154455e48b7220d680090fc754e
Rahul Maru <rahulmaru3507@gmail.com>
2024-09-30 17:37:46 +0530

    show hetatms

--- bio.py
+++ bio.py
@@ -7,15 +7,17 @@ RAD = 0.8
 COLORS = {'C': vp.vector(0.42, 0.42, 0.42),
 		  'N': vp.vector(0.04, 0.02, 0.99),
 		  'O': vp.vector(0.99, 0.0, 0.01),
-		  'S': vp.vector(0.98, 0.5, 0.01)}
+		  'S': vp.vector(0.98, 0.5, 0.02),
+		  'HETATM': vp.vector(0.85, 0.14, 0.85)}
 
-with open("2bv6.pdb") as file:
+with open("1rwt.pdb") as file:
 	lines = file.readlines()
 
-# extract only the ATOM lines from the database, and their coords
+# extract only the ATOM and HETATM lines from the database and their coords
 atoms = list(filter(lambda l : l[:4] == "ATOM", lines))
+hetatms = list(filter(lambda l : l[:6] == "HETATM", lines))
 x, y, z = [[float(atom[i:i+8]) for atom in atoms] for i in range(30, 54, 8)]
-
+hx, hy, hz = [[float(hetatm[i:i+8]) for hetatm in hetatms] for i in range(30, 54, 8)]
 # sort atoms by element
 c, o, n, s = [list(filter(lambda x: x[77] == e, atoms)) for e in ['C', 'O', 'N', 'S']]
 
@@ -26,12 +28,11 @@ c, o, n, s = [list(filter(lambda x: x[77] == e, atoms)) for e in ['C', 'O', 'N',
 # sx, sy, sz = [[float(atom[i:i+8]) for atom in s] for i in range(30, 54, 8)]
 
 print(len(atoms), " Atoms:")
-print(len(c), " Carbon,")
-print(len(n), " Nitrogen,")
-print(len(o), " Oxygen,")
-print(len(s), " Sulfur")
-
-print(vp.scene.camera.pos, vp.scene.camera.axis)
+print(len(c), " Carbon (grey),")
+print(len(n), " Nitrogen (blue),")
+print(len(o), " Oxygen (red),")
+print(len(s), " Sulfur (orange),")
+print(len(hetatms), " Hetero-atoms (magenta)")
 
 for i, atom in enumerate(atoms):
 	ele = atom[77]
@@ -40,4 +41,8 @@ for i, atom in enumerate(atoms):
 	spr.color = COLORS[ele]
 	spr.pos = vp.vector(x[i], y[i], z[i])
 
-
+for i, j, k in zip(hx, hy, hz):
+	spr = vp.sphere()
+	spr.radius = RAD
+	spr.color = COLORS['HETATM']
+	spr.pos = vp.vector(i, j, k)
