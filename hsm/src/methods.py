e2fb8b0b17f005de85af8a832ab0792896e3a100
AI-Bio <rahulmaru3507@gmail.com>
2025-04-09 15:03:37 +0530

    update binding site obtaining logic

--- hsm/src/methods.py
+++ hsm/src/methods.py
@@ -88,26 +88,31 @@ def extract_bsites(combined=False):
 	for p in pdb_ids_all:
 		prot = Protein(path + p)
 		if combined:
-			lig = prot.get_ligand('HSM')
-			bsite = prot.get_bsite(lig)
+			ligs = prot.get_ligand('HSM')
+			bsites = [prot.get_bsite(lig, True) for lig in ligs]
+
+			combined_bsite = []
+			for site in bsites:
+				for res in site:
+					if res not in combined_bsite:
+						# combines all binding sites into one, removing duplicates
+						combined_bsite.append(res)
 
 			with open(f"hsm/bsites_combined/{p[:-4]}.pdb", 'w') as f:
-					f.writelines(sum([res['atoms'] for res in bsite], []))
+				f.writelines(sum([res['atoms'] for res in combined_bsite], []))
 
 		else:
 			# create a separate file for the binding site of each chain
 			# TODO ligands in chains with no other atoms not recognized
 
-			for i in prot.chains:
-				lig = prot.get_ligand('HSM', i)
-
+			ligs = prot.get_ligand('HSM')
+			bsites = [prot.get_bsite(lig, True) for lig in ligs]
+			for i, lig in enumerate(ligs):
 				if len(lig["atoms"]) == 0:
 					continue
 			
-				bsite = prot.get_bsite(lig)
-
-				with open(f"hsm/bsites/{p[:-4]}_{i}.pdb", 'w') as f:
-					f.writelines(sum([res['atoms'] for res in bsite], []))
+				with open(f"hsm/bsites/{p[:-4]}_{lig['id'][0]}{i}.pdb", 'w') as f:
+					f.writelines(sum([res['atoms'] for res in bsites[i]], []))
 
 @timed
 def filter_bsites():
