632e1044ec006ec19deab68b82d6c8e76fdbef87
Rahul Maru <rahulmaru3507@gmail.com>
2025-09-15 12:06:32 +0530

    how many missing ents come from missing pdbs

--- filt/dat/missing_entys.py
+++ filt/dat/missing_entys.py
@@ -1,15 +1,26 @@
 import json
+import os
 
 
 cluster_ents = set(json.load(open("filt/dat/clusters-by-entity-70.json")))
 
-with open("filt/dat/struct/ents.txt", "r") as f:
+with open("filt/dat/struct/ents.txt") as f:
 	pdb_ents = set(f.readlines())
 
-pdb_ents = {ent.strip() for ent in pdb_ents}
+pdb_ents = {ent.strip().upper() for ent in pdb_ents}
 
 
 missing_entys = cluster_ents-pdb_ents
 
 with open("filt/dat/missing_entys.txt", "w") as f_out:
 	f_out.write(str(missing_entys))
+
+missing_entys_pdbs = {ent[:4] for ent in missing_entys}
+
+INDIR = "pdb"
+missing_pdbs = set()
+for ent in missing_entys_pdbs:
+	if not os.path.exists(f"{INDIR}/{ent}"):
+		missing_pdbs.add(ent)
+
+print(len(missing_entys), len(missing_entys_pdbs), len(missing_pdbs))
