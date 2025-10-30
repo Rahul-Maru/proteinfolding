55aa0107e920f8d4a10a130663780e80ed4cb5b2
AI-Bio <rahulmaru3507@gmail.com>
2025-10-30 20:22:08 +0530

    move old/ -> Old/, fix bug in gridbox formula, use full protein as receptor instead of just bsite

--- hsm/src/autodock/boxsize.py
+++ hsm/src/autodock/boxsize.py
@@ -1,17 +1,26 @@
+from math import ceil
 import bio
 
-def boxsize(fp, s, p):
-	"""Calculates an appropriate size for the autodock grid box given a receptor file path"""
+def boxsize(fp, s=0.375, p=0):
+	"""Calculates an appropriate size for the autodock grid box given a receptor file path,
+	point spacing (defaults to `0.375`), and padding on each side (in Å, defaults to `0`).
+	Returns (center, size)."""
 
 	prot = bio.Protein(fp)
 	cx, cy, cz = prot.centroid(qtype="atm")
 	x, y, z = prot.get_coords()
 
-	dx = round((max(max(x) - cx, cx - min(x)) + p) / (2*s)) * 2
-	dy = round((max(max(y) - cy, cy - min(y)) + p) / (2*s)) * 2
-	dz = round((max(max(z) - cz, cz - min(z)) + p) / (2*s)) * 2
+	def npts(k, ck):
+		"""Returns the required number of points in the k-dimension"""
+		# finds the max of the farthest +ve dist and -ve dist from the centroid ck,
+		#	adds padding dist, divides by spacing, rounds, and doubles to make it even for autogrid
+		return ceil((max(max(k) - ck, ck - min(k)) + p) / (s)) * 2
+
+	dx = npts(x, cx)
+	dy = npts(y, cy)
+	dz = npts(z, cz)
 
 	return (f"{cx},{cy},{cz}", f"{dx},{dy},{dz}")
 
 if __name__ == "__main__":
-	print(boxsize("hsm/bsites/2x45_C7.pdb"))
+	print(boxsize("hsm/bsites/2x45_C7.pdb", p=4))
--- hsm/src/autodock/main.py
+++ hsm/src/autodock/main.py
@@ -24,16 +24,14 @@ autodir = f"{ROOT}/tools/autodock"
 mgldir = f"{ROOT}/tools/mgltools/MGLToolsPckgs/AutoDockTools/Utilities24"
 
 inf = f"{ROOT}/outs/FLAPP/alignlist2.txt"
-if usr == 'rahul':
-	indir = f"{ROOT}/tools/FLAPP/sites"
-elif usr == 'aibio':
-	indir = f"{ROOT}/../filt/dat/struct/representative-binding-sites"
+sitedir = f"{ROOT}/../filt/dat/struct/representative-binding-sites"
+pdbdir = f"{ROOT}/../filt/dat/struct/pdb"
 
 lig = f"{autodir}/hsm.pdbqt"
 
 # params
 pt_space = 0.375
-padding = 12 # Å
+padding = 4 # Å
 
 runs = 25
 pop = 150
@@ -43,7 +41,11 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
+currsite = ""
+
 def main():
+	global currsite
+
 	if rank == 0:
 		print(f"MPI Initiated. Running with {size} cores. \n")
 
@@ -63,18 +65,23 @@ def main():
 
 		if rank == 0:
 			print("————————————————\n")
+			abs_st_t = time.time()
 
 		comm.Barrier()
 
 		for c, site in enumerate(subsites):
 			site = site.strip()
-			receptor = f"{indir}/{site}"
+			currsite = site
+
+			sitef = f"{sitedir}/{site}"
+			nam = site[:7]
+			pdb = f"{pdbdir}/{site[4:6]}/{nam}.ent"
 
 			outdir = f"{ROOT}/outs/autodock/{site[:-4]}"
 			os.makedirs(outdir, exist_ok=True)
 
 			# output files
-			outf = f"{outdir}/site.pdbqt"
+			recf = f"{outdir}/site.pdbqt"
 			gpf = f"{outdir}/1.gpf"
 			glg = f"{outdir}/1.glg"
 			dpf = f"{outdir}/1.dpf"
@@ -84,51 +91,58 @@ def main():
 			if os.path.exists(dlg) and os.path.getsize(dlg) > 0:
 				continue
 
-			# navigate to the output directory and temporarily copy the ligand there
+			# navigate to the output directory and temporarily copy the ligand there to match with the dpf file
 			os.chdir(outdir)
 			shutil.copy(lig, ".")
-
+			# convert .ent into .pdb
+			run(f"cp {pdb} {nam}.pdb")
 
 			print(f"{rank}.{c}) processing site: {site}")
-			site_start_time = time.time()
+			site_st_t = time.time()
 
 			try:
 				x = "preparing receptor"
 				# prepare receptor
-				run(f"pythonsh {mgldir}/prepare_receptor4.py -r {receptor} -o {outf} -A checkhydrogens")
+				run(f"pythonsh {mgldir}/prepare_receptor4.py -r {nam}.pdb -o {recf} -A checkhydrogens")
 
 				x = "preparing grid file"
-				# grid
-				cent, dims = boxsize(receptor, pt_space, padding)
+				# grid (boxed around receptor)
+				cent, dims = boxsize(sitef, pt_space, padding)
 #				print(f"{site} ({rank}): grid center, size: ", cent, dims)
-				run(f"pythonsh {mgldir}/prepare_gpf4.py -l {lig} -r {outf} -p npts={dims} -p gridcenter={cent} -o {gpf}")
+				run(f"pythonsh {mgldir}/prepare_gpf4.py -l {lig} -r {recf} -p npts={dims} -p gridcenter={cent} -o {gpf}")
 
 				x = "running autogrid"
 				run(f"{autodir}/autogrid4 -p {gpf} -l {glg}")
 
 				x = "preparing dock"
 				# docking
-				run(f"pythonsh {mgldir}/prepare_dpf42.py -l {lig} -r {outf} -p ga_run={runs} -p ga_pop_size={pop} -p ga_num_evals={evals} -o {dpf}")
+				run(f"pythonsh {mgldir}/prepare_dpf42.py -l {lig} -r {recf} -p ga_run={runs} -p ga_pop_size={pop} -p ga_num_evals={evals} -o {dpf}")
 
 				x = "docking"
 				run(f"{autodir}/autodock4 -p {dpf} -l {dlg}")
 
 				os.remove("hsm.pdbqt")
+				os.remove(f"{nam}.pdb")
+
 			except Exception as e:
 				os.remove("hsm.pdbqt")
+				os.remove(f"{nam}.pdb")
+
 				failed.append(site)
 				print(f"\033[093m{type(e).__name__} in {site}, while {x}, skipping\033[0m")
 				continue
 
-			site_end_time = time.time()
-			site_elapsed = site_end_time - site_start_time
-			print(f"{rank}.{c}) Site {site} completed in {site_elapsed:.2f} seconds\n")
+			site_end_t = time.time()
+			print(f"{rank}.{c}) Site {site} completed in {site_end_t - site_st_t:.2f}s\n")
 
-	print(f"completed on core {rank}.")
+	print(f"core {rank} finished")
 
 	dat = comm.gather(failed)
 
 	if rank == 0:
+		abs_end_t = time.time()
+		print(f"———all cores completed in {abs_end_t - abs_st_t:.2f}s———\n")
+
 		allfailed = sum(dat, [])
 		with open(f"{ROOT}/outs/autodock/failed.txt", 'w') as f2:
 			f2.write('\n'.join(allfailed))
@@ -136,6 +150,8 @@ def main():
 
 
 def run(cmd, **kwargs):
+	"""Runs a command and processes error handling.
+	"""
 	try:
 		result = subprocess.run(
 			shlex.split(cmd),
@@ -148,18 +164,18 @@ def run(cmd, **kwargs):
 			print(result.stdout)
 
 		if result.returncode != 0:
-			print(f"\033[31m[ERROR IN CORE {rank}]\033[0m")
+			print(f"\033[31m[ERROR IN CORE {rank} - {currsite}]\033[0m")
 			print(f"command {cmd} failed with error code {result.returncode}")
 			print(result.stderr)
 #			comm.Abort(42)
 			raise Exception
 
 		if "WARNING" in result.stderr:
-			print(f"\033[93m[WARNING IN CORE {rank}]\033[0m")
+			print(f"\033[93m[WARNING IN CORE {rank} - {currsite}]\033[0m")
 			print(result.stderr)
 
 	except subprocess.CalledProcessError as e:
-		print(f"\033[31m[ERROR IN CORE {rank}]\033[0m")
+		print(f"\033[31m[ERROR IN CORE {rank} - {currsite}]\033[0m")
 		print(f"command {cmd} failed with error code {e.returncode}")
 		if e.stderr:
 			# these errors are likely due to malformed geometry and can be skipped
