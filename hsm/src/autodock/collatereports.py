22bc416b017683510fb99f79d03c0db9c18da149
Rahul Maru <rahulmaru3507@gmail.com>
2026-03-31 12:58:47 +0530

    implement collatereports.py

--- hsm/src/autodock/collatereports.py
+++ hsm/src/autodock/collatereports.py
@@ -1,7 +1,28 @@
 import json
 import os
-from subprocesses import run
 
+out = {}
 for F in os.listdir("hsm/outs/autodock/reports"):
 		with open(F) as f:
-			break
+			table = False
+			pdb = ""
+			interaction = ""
+			for l in f:
+				if 'pdb' in l:
+					pdb = l[62:-14]
+					print(pdb)
+					out[pdb] = {}
+
+				elif l.startswith('**'):
+					table = False
+					interaction = l[2:-2]
+					print(interaction)
+					out[pdb][interaction] = []
+
+				elif l.startswith('+=='):
+					table = True
+
+				elif l.startswith('| ') and table:
+					out[pdb][interaction].append(l)
+
+json.dump(out, open("hsm/outs/autodock/reports.json", "w"))
